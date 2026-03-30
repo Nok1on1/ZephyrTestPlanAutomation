d2cfdb812ad319f62984eba4cdb9482ca4894dd5
Nok1on1 <giomiqa2@gmail.com>
2026-03-30 11:02:59 +0400

    fix: update API URLs to remove regional endpoint prefix

--- data/constants.ts
+++ data/constants.ts
@@ -1,29 +1,29 @@
 export const GET_TEST_PLAN_URL = (testPlanIdOrKey: string) => {
-    return `https://eu.api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}`;
+    return `https://api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}`;
 }
 
 export const GET_TEST_CYCLE_URL = (testCycleIdOrKey: string) => {
-    return `https://eu.api.zephyrscale.smartbear.com/v2/testcycles/${testCycleIdOrKey}`;
+    return `https://api.zephyrscale.smartbear.com/v2/testcycles/${testCycleIdOrKey}`;
 }
 
 export const GET_TEST_CASE_URL = (testCaseIdOrKey: string) => {
-    return `https://eu.api.zephyrscale.smartbear.com/v2/testcases/${testCaseIdOrKey}`;
+    return `https://api.zephyrscale.smartbear.com/v2/testcases/${testCaseIdOrKey}`;
 }
 
 export const GET_TEST_EXECUTIONS_URL = (testCycleIdOrKey: string) => {
-    return `https://eu.api.zephyrscale.smartbear.com/v2/testexecutions?testCycle=${testCycleIdOrKey}`;
+    return `https://api.zephyrscale.smartbear.com/v2/testexecutions?testCycle=${testCycleIdOrKey}`;
 }
 
 export const PUT_TEST_PLAN_URL = (testPlanIdOrKey: string) => {
-    return `https://eu.api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}`;
+    return `https://api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}`;
 }
 
 export const CREATE_TEST_CYCLE_LINK_URL = (testPlanIdOrKey: string) => {
-    return `https://eu.api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}/links/testcycles`
+    return `https://api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}/links/testcycles`
 }
 
 export const CREATE_ISSUE_LINK_URL = (testPlanIdOrKey: string) => {
-    return `https://eu.api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}/links/issues`;
+    return `https://api.zephyrscale.smartbear.com/v2/testplans/${testPlanIdOrKey}/links/issues`;
 }
 
-export const CREATE_PLAN = `https://eu.api.zephyrscale.smartbear.com/v2/testplans`;
+export const CREATE_PLAN = `https://api.zephyrscale.smartbear.com/v2/testplans`;
